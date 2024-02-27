fbb1c8cf82548929fb7432c1b3da3f78e98038df
Sungjun Park <espoir618@gmail.com>
2024-02-27 22:17:07 +0900

    240227 Jun Nav Container and Dark Mode

---
+++ components/common/AdminNav.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface Props {}
+
+const AdminNav: FC<Props> = (props): JSX.Element => {
+  return (
+    <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark">
+      AdminNav
+    </nav>
+  );
+};
+
+export default AdminNav;
