b10fdb355eb5f1cd418e7423b4d7725ada880067
Sungjun Park <espoir618@gmail.com>
2024-02-28 23:35:03 +0900

    240228 Jun Add App Logo

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import { FC } from "react";
+import Logo from "./Logo";
 
 interface Props {}
 
 const AdminNav: FC<Props> = (props): JSX.Element => {
   return (
     <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark">
-      AdminNav
+      <Link href="/admin" className="flex items-center space-x-2 p-3">
+        <Logo className="bg-black fill-highlight-dark dark:bg-white dark:fill-highlight-light  w-5 h-5" />
+        <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
+          Admin
+        </span>
+      </Link>
     </nav>
   );
 };
