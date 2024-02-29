a3da1f5c2150f48662a5640513cf653d1495d25d
Sungjun Park <espoir618@gmail.com>
2024-03-01 00:51:54 +0900

    240301 Jun Install react-icons

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 import { FC } from "react";
+import {
+  AiOutlineDashboard,
+  AiOutlineContainer,
+  AiOutlineTeam,
+  AiOutlineMail,
+} from "react-icons/ai";
 import Logo from "./Logo";
 
 interface Props {}
@@ -7,12 +13,17 @@ interface Props {}
 const AdminNav: FC<Props> = (props): JSX.Element => {
   return (
     <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark">
+      {/* logo */}
       <Link href="/admin" className="flex items-center space-x-2 p-3">
         <Logo className="bg-black fill-highlight-dark dark:bg-white dark:fill-highlight-light  w-5 h-5" />
         <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
           Admin
         </span>
       </Link>
+
+      {/* nav items */}
+
+      {/* nav toggler (button) */}
     </nav>
   );
 };
