8b4db973d2c0fef199f184da90c9904bb908986e
Sungjun Park <espoir618@gmail.com>
2024-03-03 18:22:17 +0900

    240303 Jun Install text editor (tiptap)

---
+++ components/editor/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+interface Props {}
+
+const Editor: FC<Props> = (props): JSX.Element => {
+  const editor = useEditor({ extensions: [StarterKit] });
+  return (
+    <div>
+      <button
+        onClick={() => {
+          if (!editor) return;
+          editor?.chain().focus().toggleBold().run();
+        }}
+      >
+        Bold
+      </button>
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
+
+export default Editor;
