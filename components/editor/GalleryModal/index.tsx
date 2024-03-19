336a0e1364a44131159ad7c337a77312c4dcb80c
Sungjun Park <espoir618@gmail.com>
2024-03-19 22:39:22 +0900

    240319 Jun Add Image upload input

--- components/editor/GalleryModal/index.tsx
+++ components/editor/GalleryModal/index.tsx
@@ -3,6 +3,7 @@ import ModalContainer, { ModalProps } from "@/components/common/ModalContainer";
 import Gallery from "./Gallery";
 import Image from "next/image";
 import ActionButton from "@/components/common/ActionButton";
+import { AiOutlineCloudUpload } from "react-icons/ai";
 
 interface Props extends ModalProps {}
 
@@ -106,6 +107,19 @@ const GalleryModal: FC<Props> = ({ visible, onClose }): JSX.Element => {
           {/* image selection and upload */}
           <div className="basis-1/4 px-2">
             <div className="space-y-4">
+              <div>
+                <input hidden type="file" id="image-input" />
+                <label htmlFor="image-input">
+                  <div
+                    className="w-full border-2 border-action
+                    text-action flex items-center justify-center
+                    space-x-2 p-2 cursor-pointer rounded"
+                  >
+                    <AiOutlineCloudUpload />
+                    <span>Upload Image</span>
+                  </div>
+                </label>
+              </div>
               {selectedImage ? (
                 <>
                   <textarea
