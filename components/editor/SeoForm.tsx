e1049a6c2d70ebcf6300585933ef532c8f4cb097
Sungjun Park <espoir618@gmail.com>
2024-04-01 22:30:56 +0900

    [Jun] Create Seo Form State

--- components/editor/SeoForm.tsx
+++ components/editor/SeoForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, FC } from "react";
+import { ChangeEventHandler, FC, useState } from "react";
 import classnames from "classnames";
 
 interface Props {}
@@ -7,6 +7,18 @@ const commonInput =
   "w-full bg-transparent outline-none border-2 border-secondary-dark focus:border-primary-dark focus:dark:border-primary rounded transition text-primary-dark dark:text-primary p-2";
 
 const SEOForm: FC<Props> = (props): JSX.Element => {
+  const [values, setValues] = useState({ meta: "", slug: "", tags: "" });
+
+  const handleChange: ChangeEventHandler<
+    HTMLTextAreaElement | HTMLInputElement
+  > = ({ target }) => {
+    let { name, value } = target;
+    if (name === "meta") value = value.substring(0, 150);
+    setValues({ ...values, [name]: value });
+  };
+
+  const { meta, slug, tags } = values;
+
   return (
     <div className="space-y-4">
       <h1
@@ -16,16 +28,31 @@ const SEOForm: FC<Props> = (props): JSX.Element => {
         SEO Section
       </h1>
 
-      <SeoInput name="slug" placeholder="slug-goes-here" label="Slug:" />
-      <SeoInput name="tags" placeholder="React, Next JS" label="Tags:" />
+      <SeoInput
+        value={slug}
+        onChange={handleChange}
+        name="slug"
+        placeholder="slug-goes-here"
+        label="Slug:"
+      />
+      <SeoInput
+        value={tags}
+        onChange={handleChange}
+        name="tags"
+        placeholder="React, Next JS"
+        label="Tags:"
+      />
 
       <div className="relative">
         <textarea
+          name="meta"
+          value={meta}
+          onChange={handleChange}
           className={classnames(commonInput, "text-lg h-20 resize-none")}
           placeholder="Meta description 150 characters will be fine"
         ></textarea>
         <p className="absolute bottom-3 right-3 text-primary-dark dark:text-primary text-sm">
-          0/150
+          {meta.length}/150
         </p>
       </div>
     </div>
