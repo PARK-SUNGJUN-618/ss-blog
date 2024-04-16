7716db1d5e8fc747c0585756d15f6c41b68877d3
Sungjun Park <espoir618@gmail.com>
2024-04-16 23:24:58 +0900

    [Jun] Apply AppHead on AdminLayout

--- pages/admin/post/create.tsx
+++ pages/admin/post/create.tsx
@@ -1,26 +1,20 @@
 import Editor from "@/components/editor";
+import AdminLayout from "@/components/layout/AdminLayout";
 import { NextPage } from "next";
 
 interface Props {}
 
 const Create: NextPage<Props> = () => {
   return (
-    <div className="max-w-4xl mx-auto">
-      <Editor
-        onSubmit={(post) => {
-          console.log(post);
-        }}
-        initialValue={{
-          title: "This is from create",
-          content: "<h2>I am Header</h2>",
-          meta: "little meta description",
-          slug: "this-is-from-create",
-          tags: "javascript",
-          thumbnail:
-            "https://res.cloudinary.com/ss-blog/image/upload/v1711363577/ss-blogs/q6k6sica6ctzvu3jcyxv.png",
-        }}
-      />
-    </div>
+    <AdminLayout title="New Post">
+      <div className="max-w-4xl mx-auto">
+        <Editor
+          onSubmit={(post) => {
+            console.log(post);
+          }}
+        />
+      </div>
+    </AdminLayout>
   );
 };
 
