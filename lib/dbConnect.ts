f7a1736193fbd24e850bd27c07ea0bf505ca972f
Sungjun Park <espoir618@gmail.com>
2024-04-18 23:21:02 +0900

    [Jun] Create api/posts/index.ts to make handler

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -5,7 +5,6 @@ const uri = process.env.MONGODB_URL as string;
 const dbConnect = async () => {
   try {
     const connection = await mongoose.connect(uri);
-    console.log(connection);
     return connection;
   } catch (error) {
     console.log("db connection fail: ", error);
