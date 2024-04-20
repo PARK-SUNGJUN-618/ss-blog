46bcd493dab45509079aa159c568c94f975ad509
Sungjun Park <espoir618@gmail.com>
2024-04-20 20:39:22 +0900

    [Jun] Install Joi for schema description

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,5 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import { NextApiHandler } from "next";
+import Joi from "joi";
 
 const handler: NextApiHandler = async (req, res) => {
   const { method } = req;
@@ -8,7 +9,22 @@ const handler: NextApiHandler = async (req, res) => {
       await dbConnect();
       res.json({ ok: true });
     }
+    case "POST":
+      return createNewPost(req, res);
   }
+};
+
+const createNewPost: NextApiHandler = (req, res) => {
+  const { body } = req;
+
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(body);
+  console.log(error);
+
+  res.json({ ok: true });
 };
 
 export default handler;
