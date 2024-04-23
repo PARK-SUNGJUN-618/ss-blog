cb5c2140c1e95cee55f44b9e38e5c21ce283d078
Sungjun Park <espoir618@gmail.com>
2024-04-23 23:36:46 +0900

    [Jun] change error code to accepting formData

--- pages/admin/post/create.tsx
+++ pages/admin/post/create.tsx
@@ -7,18 +7,24 @@ interface Props {}
 
 const Create: NextPage<Props> = () => {
   const handleSubmit = async (post: FinalPost) => {
-    // generate FormData
-    const formData = new FormData();
-    for (let key in post) {
-      const value = (post as any)[key];
-      if (key === "tags") {
-        const tags = value.split(",").map((tag: string) => tag.trim());
-        formData.append("tags", JSON.stringify(tags));
-      } else formData.append(key, value);
+    try {
+      // generate FormData
+      const formData = new FormData();
+      for (let key in post) {
+        const value = (post as any)[key];
+        if (key === "tags") {
+          const tags = value.split(",").map((tag: string) => tag.trim());
+          formData.append("tags", JSON.stringify(tags));
+          console.log("JSON.stringify(tags):", JSON.stringify(tags));
+        } else formData.append(key, value);
+      }
+      // submit our post
+      console.log("formData:", formData);
+      const { data } = await axios.post("/api/posts", formData);
+      console.log(data);
+    } catch (error: any) {
+      console.log(error.response.data);
     }
-
-    // submit our post
-    const { data } = await axios.post("/api/posts", formData);
   };
 
   return (
--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -2,6 +2,11 @@ import dbConnect from "@/lib/dbConnect";
 import { NextApiHandler } from "next";
 import Joi from "joi";
 import { postValidationSchema, validateSchema } from "@/lib/validator";
+import { readFile } from "@/lib/utils";
+
+export const config = {
+  api: { bodyParser: false },
+};
 
 const handler: NextApiHandler = async (req, res) => {
   const { method } = req;
@@ -15,9 +20,14 @@ const handler: NextApiHandler = async (req, res) => {
   }
 };
 
-const createNewPost: NextApiHandler = (req, res) => {
-  const { body } = req;
+const createNewPost: NextApiHandler = async (req, res) => {
+  const { files, body } = await readFile(req);
 
+  // let tags = [];
+  // if (body.tags) tags = body.tags;
+
+  // const error = validateSchema(postValidationSchema, { ...body, tags });
+  console.log("body", body);
   const error = validateSchema(postValidationSchema, body);
   if (error) return res.status(400).json({ error });
 
