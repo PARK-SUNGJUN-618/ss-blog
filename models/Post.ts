a538a58cc71e4de0762535fea85fd5c335257b48
Sungjun Park <espoir618@gmail.com>
2024-04-27 11:52:07 +0900

    [Jun] Types for Schema

--- models/Post.ts
+++ models/Post.ts
@@ -1,8 +1,18 @@
-import { Schema, models, model } from "mongoose";
+import { Schema, models, model, ObjectId, Model } from "mongoose";
 
 // title, content, slug, tags, thumbnail, meta, author, date,
+interface PostModelSchema {
+  title: string;
+  slug: string;
+  meta: string;
+  content: string;
+  tags: string[];
+  thumbnail: { url: string; public_id: string };
+  author: ObjectId;
+  createdAt: Date;
+}
 
-const PostSchema = new Schema(
+const PostSchema = new Schema<PostModelSchema>(
   {
     title: {
       type: String,
@@ -45,4 +55,4 @@ const PostSchema = new Schema(
 
 const Post = models?.Post || model("Post", PostSchema);
 
-export default Post;
+export default Post as Model<PostModelSchema>;
