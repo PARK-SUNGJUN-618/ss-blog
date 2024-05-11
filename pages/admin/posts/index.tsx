5c9e4dff301a8789e2e95060910e699e3e8a328a
Sungjun Park <espoir618@gmail.com>
2024-05-12 01:19:38 +0900

    [Jun] Apply Infinite scroll posts - Admin

--- pages/admin/posts/index.tsx
+++ pages/admin/posts/index.tsx
@@ -1,7 +1,9 @@
+import InfiniteScrollPosts from "@/components/common/InfiniteScrollPosts";
 import PostCard from "@/components/common/PostCard";
 import AdminLayout from "@/components/layout/AdminLayout";
 import { formatPosts, readPostsFromDb } from "@/lib/utils";
 import { PostDetail } from "@/utils/types";
+import axios from "axios";
 import {
   GetServerSideProps,
   InferGetServerSidePropsType,
@@ -15,63 +17,35 @@ type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
 let pageNo = 0;
 const limit = 9;
 
-// const posts = [
-//   {
-//     title: "240505_4_title",
-//     slug: "240505_4_title",
-//     content: "<p>240505_4_content</p>",
-//     meta: "240505_4_descriptio n240505_4_description240 505_4_description 240505_4_description2 40505_4_descripti on240505_4_des cription2405 05_4_description240505_4_description240505_4_description",
-//     tags: ["240505_4_1", "240505_4_2"],
-//     thumbnail:
-//       "https://res.cloudinary.com/ss-blog/image/upload/v1714903596/ssblog/e1yvrxh5rclajlt7mgf3.png",
-//     createdAt: "Tue Oct 11 2022 19:28:49 GMT+0900 (Japan Standard Time)",
-//   },
-//   {
-//     title: "240505_4_title2",
-//     slug: "240505_4_title2",
-//     content: "<p>240505_4_content2</p>",
-//     meta: "240505_4_description2",
-//     tags: ["240505_4_12", "240505_4_22"],
-//     thumbnail:
-//       "https://res.cloudinary.com/ss-blog/image/upload/v1714903596/ssblog/e1yvrxh5rclajlt7mgf3.png",
-//     createdAt: "Tue Oct 11 2022 19:28:49 GMT+0900 (Japan Standard Time)",
-//   },
-//   {
-//     title: "240505_4_title3",
-//     slug: "240505_4_title3",
-//     content: "<p>240505_4_content3</p>",
-//     meta: "240505_4_description3",
-//     tags: ["240505_4_13", "240505_4_23"],
-//     thumbnail:
-//       "https://res.cloudinary.com/ss-blog/image/upload/v1714903596/ssblog/e1yvrxh5rclajlt7mgf3.png",
-//     createdAt: "Tue Oct 11 2022 19:28:49 GMT+0900 (Japan Standard Time)",
-//   },
-//   {
-//     title: "240505_4_title4",
-//     slug: "240505_4_title4",
-//     content: "<p>240505_4_content4</p>",
-//     meta: "240505_4_description4",
-//     tags: ["240505_4_14", "240505_4_24"],
-//     thumbnail:
-//       "https://res.cloudinary.com/ss-blog/image/upload/v1714903596/ssblog/e1yvrxh5rclajlt7mgf3.png",
-//     createdAt: "Tue Oct 11 2022 19:28:49 GMT+0900 (Japan Standard Time)",
-//   },
-// ];
-
 const Posts: NextPage<Props> = ({ posts }) => {
   const [postsToRender, setPostsToRender] = useState(posts);
+  const [hasMorePosts, setHasMorePosts] = useState(true);
+
+  const fetchMorePosts = async () => {
+    try {
+      pageNo++;
+      const { data } = await axios(
+        `/api/posts?limit=${limit}&pageNo=${pageNo}`
+      );
+      setPostsToRender([...postsToRender, ...data.posts]);
+      if (data.posts.length < limit) {
+        setHasMorePosts(false);
+      }
+    } catch (error) {
+      setHasMorePosts(false);
+      console.log(error);
+    }
+  };
 
   return (
     <AdminLayout>
-      <div className="max-w-4xl mx-auto p-3">
-        <div className="grid grid-cols-3 gap-4">
-          {postsToRender.map((post) => (
-            // <Link key={post.slug} href={"/" + post.slug}>
-            <PostCard key={post.slug} post={post} />
-            // </Link>
-          ))}
-        </div>
-      </div>
+      <InfiniteScrollPosts
+        hasMore={hasMorePosts}
+        next={fetchMorePosts}
+        dataLength={postsToRender.length}
+        posts={postsToRender}
+        showControls
+      />
     </AdminLayout>
   );
 };
