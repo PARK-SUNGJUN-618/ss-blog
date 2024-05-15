45b807e5effe9e078efd08c79fec5f854819151b
Sungjun Park <espoir618@gmail.com>
2024-05-15 19:26:11 +0900

    [Jun] Create GitHubAuthButton Component

--- components/common/nav/UserNav.tsx
+++ components/common/nav/UserNav.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import Logo from "../Logo";
 import { APP_NAME } from "../AppHead";
 import { HiLightBulb } from "react-icons/hi";
+import { GitHubAuthButton } from "@/components/button";
 
 interface Props {}
 
@@ -21,6 +22,8 @@ const UserNav: FC<Props> = (props): JSX.Element => {
         <button className="dark:text-secondary-dark text-secondary-light">
           <HiLightBulb size={34} />
         </button>
+
+        <GitHubAuthButton lightOnly />
       </div>
     </div>
   );
