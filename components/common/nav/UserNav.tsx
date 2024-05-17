893210cf85904bc79c797b6a6f508acc56a42637
Sungjun Park <espoir618@gmail.com>
2024-05-17 18:20:27 +0900

    [Jun] Create DropdownOptions on ProfileHead

--- components/common/nav/UserNav.tsx
+++ components/common/nav/UserNav.tsx
@@ -4,10 +4,16 @@ import Logo from "../Logo";
 import { APP_NAME } from "../AppHead";
 import { HiLightBulb } from "react-icons/hi";
 import { GitHubAuthButton } from "@/components/button";
+import ProfileHead from "../ProfileHead";
+import DropdownOptions, { dropDownOptions } from "../DropdownOptions";
 
 interface Props {}
 
 const UserNav: FC<Props> = (props): JSX.Element => {
+  const dropDownOptions: dropDownOptions = [
+    { label: "Dashboard", onClick() {} },
+    { label: "Logout", onClick() {} },
+  ];
   return (
     <div className="flex items-center justify-between bg-primary-dark px-3 py-1">
       {/* Logo */}
@@ -23,7 +29,12 @@ const UserNav: FC<Props> = (props): JSX.Element => {
           <HiLightBulb size={34} />
         </button>
 
-        <GitHubAuthButton lightOnly />
+        {/* <GitHubAuthButton lightOnly /> */}
+
+        <DropdownOptions
+          options={dropDownOptions}
+          head={<ProfileHead nameInitial="P" lightOnly />}
+        />
       </div>
     </div>
   );
