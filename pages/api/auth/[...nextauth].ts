21e919cee2525c308898c1922d223d7694ac2b7e
Sungjun Park <espoir618@gmail.com>
2024-05-29 19:30:18 +0900

    [Jun] Custom Auth Pages

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -40,6 +40,10 @@ const authOptions: NextAuthOptions = {
       return token;
     },
   },
+  pages: {
+    signIn: "/auth/signin",
+    error: "/404",
+  },
 };
 
 export default NextAuth(authOptions);
