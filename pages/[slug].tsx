1f85a36cec4da1ae42b1a1d1a5a63c7732d8f472
Sungjun Park <espoir618@gmail.com>
2024-06-02 13:33:18 +0900

    [Jun] Fetching Single Post

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,12 +1,23 @@
 import DefaultLayout from "@/components/layout/DefaultLayout";
 import dbConnect from "@/lib/dbConnect";
 import Post from "@/models/Post";
-import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
 
-interface Props {}
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-const SinglePost: NextPage<Props> = () => {
-  return <DefaultLayout>SinglePost</DefaultLayout>;
+const SinglePost: NextPage<Props> = ({ post }) => {
+  return (
+    <DefaultLayout>
+      {post.slug}
+      {post.title}
+      {post.content}
+    </DefaultLayout>
+  );
 };
 
 export default SinglePost;
@@ -29,4 +40,45 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 };
 
-// export const getStaticProps: GetStaticProps = () => {};
+interface StaticPropsResponse {
+  post: {
+    id: string;
+    title: string;
+    content: string;
+    meta: string;
+    tags: string[];
+    slug: string;
+    thumbnail: string;
+    createdAt: string;
+  };
+}
+
+export const getStaticProps: GetStaticProps<
+  StaticPropsResponse,
+  { slug: string }
+> = async ({ params }) => {
+  try {
+    await dbConnect();
+    const post = await Post.findOne({ slug: params?.slug });
+    if (!post) return { notFound: true };
+
+    const { _id, title, content, meta, slug, tags, thumbnail, createdAt } =
+      post;
+    return {
+      props: {
+        post: {
+          id: _id.toString(),
+          title,
+          content,
+          meta,
+          slug,
+          tags,
+          thumbnail: thumbnail?.url || "",
+          createdAt: createdAt.toString(),
+        },
+      },
+    };
+  } catch (error) {
+    return { notFound: true };
+  }
+};
