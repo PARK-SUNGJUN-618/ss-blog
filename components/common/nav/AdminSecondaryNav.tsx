4256b6dbc83ba3694d3703bc90c732da5f281676
Sungjun Park <espoir618@gmail.com>
2024-06-03 22:48:31 +0900

    [Jun] Create AdminSecondaryNav

---
+++ components/common/nav/AdminSecondaryNav.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+import DropdownOptions, { dropDownOptions } from "../DropdownOptions";
+import ProfileHead from "../ProfileHead";
+import { useRouter } from "next/router";
+import useDarkMode from "@/hooks/useDarkMode";
+import { signOut } from "next-auth/react";
+
+interface Props {}
+
+const AdminSecondaryNav: FC<Props> = (props): JSX.Element => {
+  const router = useRouter();
+  const { toggleTheme } = useDarkMode();
+  const navigateToCreateNewPost = () => router.push("/admin/posts/create");
+  const handleLogOut = async () => await signOut();
+
+  const options: dropDownOptions = [
+    {
+      label: "Add new post",
+      onClick: navigateToCreateNewPost,
+    },
+    {
+      label: "Change theme",
+      onClick: toggleTheme,
+    },
+    {
+      label: "Log out",
+      onClick: handleLogOut,
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-between">
+      {/* search bar */}
+      <input type="text" />
+      {/* options / profile head */}
+      <DropdownOptions
+        head={<ProfileHead nameInitial="P" />}
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default AdminSecondaryNav;
--- components/layout/AdminLayout.tsx
+++ components/layout/AdminLayout.tsx
@@ -10,6 +10,7 @@ import {
 } from "react-icons/ai";
 import Link from "next/link";
 import AppHead from "../common/AppHead";
+import AdminSecondaryNav from "../common/nav/AdminSecondaryNav";
 
 interface Props {
   children: ReactNode;
@@ -30,7 +31,10 @@ const AdminLayout: FC<Props> = ({ title, children }): JSX.Element => {
       <AppHead title={title} />
       <div className="flex">
         <AdminNav navItems={navItems} />
-        <div className="flex-1 p-4">{children}</div>
+        <div className="flex-1 p-4 dark:bg-primary-dark bg-primary">
+          <AdminSecondaryNav />
+          {children}
+        </div>
         {/* create button */}
         <Link
           href="/admin/posts/create"
