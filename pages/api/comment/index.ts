5dac5044dd800ad589edd3f5a9ea4de999b88a40
Sungjun Park <espoir618@gmail.com>
2024-06-11 18:24:04 +0900

    [Jun] Create Adding New Comment

--- pages/api/comment/index.ts
+++ pages/api/comment/index.ts
@@ -1,5 +1,8 @@
+import dbConnect from "@/lib/dbConnect";
 import { isAuth } from "@/lib/utils";
 import { commentValidationSchema, validateSchema } from "@/lib/validator";
+import Comment from "@/models/Comment";
+import Post from "@/models/Post";
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = (req, res) => {
@@ -20,6 +23,22 @@ const createNewComment: NextApiHandler = async (req, res) => {
 
   const error = validateSchema(commentValidationSchema, req.body);
   if (error) return res.status(422).json({ error });
+
+  // create comment
+  await dbConnect();
+  const { belongsTo, content } = req.body;
+  const post = await Post.findById(belongsTo);
+  if (!post) return res.status(401).json({ error: "Invalid Post" });
+
+  const comment = new Comment({
+    content,
+    belongsTo,
+    owner: user.id,
+    chiefComment: true,
+  });
+
+  await comment.save();
+  res.status(201).json(comment);
 };
 
 export default handler;
