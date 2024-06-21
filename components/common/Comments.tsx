18c6b66e9a57d9e8196c3697af39591c460b8c38
Sungjun Park <espoir618@gmail.com>
2024-06-21 18:41:39 +0900

    [Jun] Confirm Delete Comment

--- components/common/Comments.tsx
+++ components/common/Comments.tsx
@@ -64,6 +64,25 @@ const Comments: FC<Props> = ({ belongsTo }): JSX.Element => {
     setComments([...updatedComments]);
   };
 
+  const updateDeletedComments = (deletedComment: CommentResponse) => {
+    if (!comments) return;
+    let newComments = [...comments];
+
+    if (deletedComment.chiefComment)
+      newComments = newComments.filter(({ id }) => id !== deletedComment.id);
+    else {
+      const chiefCommentIndex = newComments.findIndex(
+        ({ id }) => id === deletedComment.repliedTo
+      );
+      const newReplies = newComments[chiefCommentIndex].replies?.filter(
+        ({ id }) => id !== deletedComment.id
+      );
+      newComments[chiefCommentIndex].replies = newReplies;
+    }
+
+    setComments([...newComments]);
+  };
+
   const handleNewCommentSubmit = async (content: string) => {
     const newComment = await axios
       .post("/api/comment", { content, belongsTo })
@@ -101,7 +120,18 @@ const Comments: FC<Props> = ({ belongsTo }): JSX.Element => {
   };
 
   const handleOnDeleteConfirm = () => {
-    console.log(commentToDelete);
+    if (!commentToDelete) return;
+
+    axios
+      .delete(`/api/comment?commentId=${commentToDelete.id}`)
+      .then(({ data }) => {
+        if (data.removed) updateDeletedComments(commentToDelete);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setCommentToDelete(null);
+        setShowConfirmModal(false);
+      });
   };
 
   useEffect(() => {
