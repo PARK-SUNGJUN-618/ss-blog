bc49c3337dd3be8d48a0dbeae6c2b5b787cee7b6
Sungjun Park <espoir618@gmail.com>
2024-07-02 18:12:17 +0900

    [Jun] Add admin Check on Search

--- pages/api/posts/search.ts
+++ pages/api/posts/search.ts
@@ -1,10 +1,13 @@
 import dbConnect from "@/lib/dbConnect";
-import { formatPosts } from "@/lib/utils";
+import { formatPosts, isAdmin } from "@/lib/utils";
 import Post from "@/models/Post";
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method !== "GET") return res.status(404).send("Not found!");
+
+  const admin = await isAdmin(req, res);
+  if (!admin) return res.status(403).json({ error: "Unauthorized access!" });
 
   const title = req.query.title as string;
   if (!title.trim())
